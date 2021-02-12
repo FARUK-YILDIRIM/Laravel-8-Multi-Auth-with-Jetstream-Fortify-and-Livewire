2a3c2aa521b2921d152a7b35b1ba1e6bcbfa7e39
frkyldrm <faruk.yildirim@outlook.com.tr>
2021-02-12 11:33:04 +0300

    Admin model and migrations

--- database/migrations/2021_02_12_081650_create_admins_table.php
+++ database/migrations/2021_02_12_081650_create_admins_table.php
@@ -15,6 +15,13 @@ class CreateAdminsTable extends Migration
     {
         Schema::create('admins', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->foreignId('current_team_id')->nullable();
+            $table->text('profile_photo_path')->nullable();
             $table->timestamps();
         });
     }
