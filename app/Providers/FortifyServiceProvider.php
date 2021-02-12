10fbbe7b392503b0a671e15719ebfe51a08e451d
frkyldrm <faruk.yildirim@outlook.com.tr>
2021-02-12 13:16:05 +0300

    Create Guards for Admin

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use Laravel\Fortify\Actions\AttemptToAuthenticate;
+use Laravel\Fortify\Actions\RedirectIfTwoFactorAuthenticatable;
+use Illuminate\Contracts\Auth\StatefulGuard;
+use App\Http\Controllers\AdminController;
 use App\Actions\Fortify\CreateNewUser;
 use App\Actions\Fortify\ResetUserPassword;
 use App\Actions\Fortify\UpdateUserPassword;
@@ -21,7 +25,11 @@ class FortifyServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->when([AdminController::class, AttemptToAuthenticate::class, RedirectIfTwoFactorAuthenticatable::class])
+        ->needs(StatefulGuard::class)
+        ->five(function(){
+            return Auth::guard('admin');
+        });
     }
 
     /**
